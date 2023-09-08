bb7a928cd960b0d10fb00d1f2adf1b7943cb0bfd
Deniz Bozyigit <deniz@scandit.com>
2023-09-08 14:17:53 +0200

    added spm package demo

---
+++ BarcodeCaptureSimpleSampleSwift/BarcodeCaptureSimpleSampleSwift/ViewController.swift
@@ -0,0 +1,166 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import ScanditBarcodeCapture
+
+extension DataCaptureContext {
+	// There is a Scandit sample license key set below here.
+	// This license key is enabled for sample evaluation only.
+	// If you want to build your own application,
+    // get your license key by signing up for a trial at https://ssl.scandit.com/dashboard/sign-up?p=test
+    private static let licenseKey = "AQIzpSC5AyYeKA6KZgjthjEmMbJBFJEpiUUjkCJu72AUVSWyGjN0xNt0OVgASxKO6FwLejYDRFGraFReiUwL8wp3a8mgX0elHhmx0JhY/QYrbQHJjGIhQAhjcW1cYr+ogWCDUmhM2KuWPlJXBkSGmbwinMAqKusC5zQHGoY6JDKJXbzv97CRhGdjlfgjhTZErgfs+P/fLp0cCCAmP+TTZ6jiyA/my9Ojy7ugt7DKay2ZAkezAO8OwAtnl0GUIflPz6KI68hRPaAV18wwS030+riqfDIcFQ+3BAfqRMpJxrYfKZOvvwyTAbC+5ZzgFmwd9YR0vbFToSmHDemEyRVufdMw0s+jqCHsCY5ox8jBfV1RkmDQxCckkJoS3rhPmLgEyiTm+gI0y30swn2orZ4aaml+aoA55vhN4jY+ZAkMkmhipAXK/TMzyHo4iUDA4/v3TgiJbodw27iI/+f6YxIpA+/nAEItRH7C3vuxAdo8lmk5q0QeCkc6QA0FhQa6S/cu8yrehTi+Lb8khFmt3gkwEubowGdg3cg8KoBsDgY59lAKWy55rmVznq7REv6ugw1KwgW724K4s5ILfgQ2NcV/jFgeTReaTSVYUWKZGXdJmDrteX7tgmdfkpjaCrijgSGwYRaATxVKitCYIPyfuipsSHdC0iLqCoJ8CIc2UclvimPXDzDLk83uIRFjgspykVm+eIsKiMuxrW6OlB7o7NWPcJtEcyO74Mq6scB8+bWP5eJFIPazUcZEtxG2u3UpWz7+EoBADwbUI9G63HcTwt2bi8JZo16pfGxsWti3DJ1HWooGSIVvyZ2jePvhBcuu+EbtOucgdPDvDTCTpm/V"
+
+    // Get a licensed DataCaptureContext.
+    static var licensed: DataCaptureContext {
+        return DataCaptureContext(licenseKey: licenseKey)
+    }
+}
+
+class ViewController: UIViewController {
+
+    private var context: DataCaptureContext!
+    private var camera: Camera?
+    private var barcodeCapture: BarcodeCapture!
+    private var captureView: DataCaptureView!
+    private var overlay: BarcodeCaptureOverlay!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupRecognition()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Switch camera on to start streaming frames. The camera is started asynchronously and will take some time to
+        // completely turn on. To be notified when the camera is completely on, pass non nil block as completion to
+        // camera?.switch(toDesiredState:completionHandler:)
+        barcodeCapture.isEnabled = true
+        camera?.switch(toDesiredState: .on)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        // Switch camera off to stop streaming frames. The camera is stopped asynchronously and will take some time to
+        // completely turn off. Until it is completely stopped, it is still possible to receive further results, hence
+        // it's a good idea to first disable barcode capture as well.
+        // To be notified when the camera is completely stopped, pass a non nil block as completion to
+        // camera?.switch(toDesiredState:completionHandler:)
+        barcodeCapture.isEnabled = false
+        camera?.switch(toDesiredState: .off)
+    }
+
+    func setupRecognition() {
+        // Create data capture context using your license key.
+        context = DataCaptureContext.licensed
+
+        // Use the world-facing (back) camera and set it as the frame source of the context. The camera is off by
+        // default and must be turned on to start streaming frames to the data capture context for recognition.
+        // See viewWillAppear and viewDidDisappear above.
+        camera = Camera.default
+        context.setFrameSource(camera, completionHandler: nil)
+
+        // Use the recommended camera settings for the BarcodeCapture mode.
+        let recommendedCameraSettings = BarcodeCapture.recommendedCameraSettings
+        camera?.apply(recommendedCameraSettings)
+
+        // The barcode capturing process is configured through barcode capture settings
+        // and are then applied to the barcode capture instance that manages barcode recognition.
+        let settings = BarcodeCaptureSettings()
+
+        // The settings instance initially has all types of barcodes (symbologies) disabled. For the purpose of this
+        // sample we enable a very generous set of symbologies. In your own app ensure that you only enable the
+        // symbologies that your app requires as every additional enabled symbology has an impact on processing times.
+        settings.set(symbology: .ean13UPCA, enabled: true)
+        settings.set(symbology: .ean8, enabled: true)
+        settings.set(symbology: .upce, enabled: true)
+        settings.set(symbology: .qr, enabled: true)
+        settings.set(symbology: .dataMatrix, enabled: true)
+        settings.set(symbology: .code39, enabled: true)
+        settings.set(symbology: .code128, enabled: true)
+        settings.set(symbology: .interleavedTwoOfFive, enabled: true)
+
+        // Some linear/1d barcode symbologies allow you to encode variable-length data. By default, the Scandit
+        // Data Capture SDK only scans barcodes in a certain length range. If your application requires scanning of one
+        // of these symbologies, and the length is falling outside the default range, you may need to adjust the "active
+        // symbol counts" for this symbology. This is shown in the following few lines of code for one of the
+        // variable-length symbologies.
+        let symbologySettings = settings.settings(for: .code39)
+        symbologySettings.activeSymbolCounts = Set(7...20) as Set<NSNumber>
+
+        // Create new barcode capture mode with the settings from above.
+        barcodeCapture = BarcodeCapture(context: context, settings: settings)
+
+        // Register self as a listener to get informed whenever a new barcode got recognized.
+        barcodeCapture.addListener(self)
+
+        // To visualize the on-going barcode capturing process on screen, setup a data capture view that renders the
+        // camera preview. The view must be connected to the data capture context.
+        captureView = DataCaptureView(context: context, frame: view.bounds)
+        captureView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(captureView)
+
+        // Add a barcode capture overlay to the data capture view to render the location of captured barcodes on top of
+        // the video preview. This is optional, but recommended for better visual feedback.
+        overlay = BarcodeCaptureOverlay(barcodeCapture: barcodeCapture, view: captureView, style: .frame)
+        overlay.viewfinder = RectangularViewfinder(style: .square, lineStyle: .light)
+    }
+
+    private func showResult(_ result: String, completion: @escaping () -> Void) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: result, message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in completion() }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+}
+
+// MARK: - BarcodeCaptureListener
+
+extension ViewController: BarcodeCaptureListener {
+
+    func barcodeCapture(_ barcodeCapture: BarcodeCapture,
+                        didScanIn session: BarcodeCaptureSession,
+                        frameData: FrameData) {
+        guard let barcode = session.newlyRecognizedBarcodes.first else {
+            return
+        }
+
+        // Stop recognizing barcodes for as long as we are displaying the result. There won't be any new results until
+        // the capture mode is enabled again. Note that disabling the capture mode does not stop the camera, the camera
+        // continues to stream frames until it is turned off.
+        barcodeCapture.isEnabled = false
+
+        // If you are not disabling barcode capture here and want to continue scanning, consider setting the
+        // codeDuplicateFilter when creating the barcode capture settings to around 500 or even -1 if you do not want
+        // codes to be scanned more than once.
+
+        // Get the human readable name of the symbology and assemble the result to be shown.
+        let symbology = SymbologyDescription(symbology: barcode.symbology).readableName
+
+        var result = "Scanned: "
+        if let data = barcode.data {
+            result += "\(data) "
+        }
+        result += "(\(symbology))"
+
+        showResult(result) { [weak self] in
+            // Enable recognizing barcodes when the result is not shown anymore.
+            self?.barcodeCapture.isEnabled = true
+        }
+    }
+
+}
